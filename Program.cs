1e9d5de7839b4bc7cae62f05210057294339314c
SebastianoMV <sebastianovisco@gmail.com>
2022-09-23 14:17:12 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+StreamReader file = File.OpenText("../../../addresses.csv");
+
+List<Address> addresses = new List<Address>();
+
+//Console.WriteLine(file.ReadToEnd());
+
+while (!file.EndOfStream)
+{
+    string line = file.ReadLine();
+    Console.WriteLine(line);
+    
+}
+
+
+file.Close();
+
+
+public class Address
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string Province { get; set; }
+    public string Zip { get; set; }
+
+
+}
+
